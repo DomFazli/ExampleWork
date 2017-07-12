7a2887a67c6623e30d115daa290206e7abe2f2da
DomFazli <domyarharris@yahoo.com>
2017-07-12 13:37:15 -0400

    scene controller script from rc spawn section script from rc
    Signed-off-by: DomFazli <domyarharris@yahoo.com>

---
+++ Scene_Controller.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Scene_Controller : MonoBehaviour
+{
+    // objects to swap activity if needed
+    GameObject _on, _off;
+
+    // name of level to load when button is clicked
+    string _level_name;
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLevel(int build_index)
+    {
+        PlayerPrefs.Save();
+
+        // reset time scale if we were paused
+        Time.timeScale = 1;
+
+        if (build_index < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(build_index);
+        else
+            Debug.Log("Scene index out of range.");
+    }
+    public void LoadLevel(string name)
+    {
+        PlayerPrefs.Save();
+        Time.timeScale = 1;
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadMainMenu()
+    {
+        LoadLevel("MainMenu");
+    }
+
+    public void SwapObjects()
+    {
+        _off.SetActive(false);
+        _on.SetActive(true);
+    }
+
+    // set the object to set active
+    public void SetObjectOn(GameObject on)
+    {
+        _on = on;
+    }
+
+    // set the object to deactivate
+    public void SetObjectOff(GameObject off)
+    {
+        _off = off;
+    }
+
+    public void SetLevelToPlay(string level_name)
+    {
+        _level_name = level_name;
+    }
+
+    public void PlayButtonClick()
+    {
+        LoadLevel(_level_name);
+    }
+}
---
+++ SpawnSection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSection : MonoBehaviour
+{
+    public GameObject player;
+
+    private float previousSectionX;         // posX of last section spawned
+    private Queue<GameObject> sections;     // list of sections to be destroyed
+    private int numPrefabs = 5;             // number of sections in folder
+
+    // Use this for initialization
+    void Start()
+    {
+        // spawn the first section and store it so we can spawn a new section next to it
+        GameObject section = (GameObject)Instantiate(Resources.Load("Sections/Section0"), Vector3.zero, Quaternion.identity);
+
+        previousSectionX = section.transform.position.x;
+
+        // init queue and add first section to be destroyed
+        sections = new Queue<GameObject>();
+        sections.Enqueue(section);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // the distance between the player and the last section spawned, add a new random section
+        if (previousSectionX - player.transform.position.x < 100)
+        {
+            // instantiate a random section from our assets and save it as previous
+            GameObject section = (GameObject)Instantiate(
+                Resources.Load("Sections/Section" + Random.Range(0, numPrefabs).ToString()),    // the object to load
+                new Vector3(previousSectionX + 61.32f, 0, 0),                                   // position
+                Quaternion.identity);                                                           // rotation
+
+            previousSectionX = section.transform.position.x;
+
+            // add to queue to be destroyed
+            sections.Enqueue(section);
+        }
+
+        // the distance between the player and the section behind the player, destroy section
+        if (player.transform.position.x - sections.Peek().transform.position.x > 100)
+        {
+            Destroy(sections.Dequeue());
+        }
+    }
+}
